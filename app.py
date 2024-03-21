de24b7d3b39131d010cf2dbc7d2b1b061b0dc73f
AntonBarslund <a.barslundd@gmail.com>
2024-03-21 15:19:27 +0100

    route

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from openai import OpenAI
+import subprocess
 
 app = Flask(__name__)
 
+client = OpenAI()
 
-@app.route('/templates')
+@app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
